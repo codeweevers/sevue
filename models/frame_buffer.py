a92503b0328a78eaff157543d90a9b4b83bd5d05
adxxali <adxxali@gmail.com>
2026-03-11 14:10:55 +0530

    Video Handling Fixes
    Added edge detection
    Auto exposure
    Changed running mode to VIDEO

--- models/frame_buffer.py
+++ models/frame_buffer.py
@@ -1,27 +1,20 @@
 ﻿import threading
 
-import cv2
-
-from constants import AI_FRAME_SIZE
-
-
 class FrameBuffer:
     def __init__(self):
         self.lock = threading.Lock()
         self.frame = None
-        self.width = 0
-        self.height = 0
-        self.ai_w, self.ai_h = AI_FRAME_SIZE
 
-    def push(self, frame):
+    def push_latest(self, frame):
         with self.lock:
-            self.frame = frame.copy()
-            self.height, self.width = frame.shape[:2]
+            self.frame = frame
 
     def get_ai(self):
         with self.lock:
-            if self.frame is None:
-                return None
-            return cv2.cvtColor(
-                cv2.resize(self.frame, (self.ai_w, self.ai_h)), cv2.COLOR_BGR2RGB
-            )
+            frame = self.frame
+            self.frame = None
+            return frame
+
+    def has_frame(self):
+        with self.lock:
+            return self.frame is not None
